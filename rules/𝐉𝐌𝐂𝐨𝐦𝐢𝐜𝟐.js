2ca295dae4e3080d2bc08f6a6cc61cddcb67c5e7
csdown <3082291997@qq.com>
2025-04-14 22:03:53 +0800

    Update 𝐉𝐌𝐂𝐨𝐦𝐢𝐜𝟐.js

--- rules/𝐉𝐌𝐂𝐨𝐦𝐢𝐜𝟐.js
+++ rules/𝐉𝐌𝐂𝐨𝐦𝐢𝐜𝟐.js
@@ -267,7 +267,7 @@ const csdown = {
                 for (item of data.images) {
                     var picUrl = getItem('img_host') + '/media/photos/' + input + '/' + item;
                     let decode = $('').image((picUrl) => {
-                        return $.require("csdown?rule=" + MY_TITLE).decodeimg(picUrl, input);
+                        return $.require("csdown?rule=" + MY_RULE.title).decodeimg(picUrl, input);
                     }, picUrl);
                     img.push(picUrl + decode)
                 }
