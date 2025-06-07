94a69ac94d3caebda4ff716758ced2d4e59beb6d
zousizhe <37259360+zousizhe@users.noreply.github.com>
2025-06-07 15:12:40 +0800

    Update 茄子.js

--- rules/茄子.js
+++ rules/茄子.js
@@ -1,7 +1,7 @@
 const csdown = {
     d: [],
     author: '流苏',
-    version: '20250520_2',
+    version: '20250521',
     rely: (data) => {
         return data.match(/\{([\s\S]*)\}/)[0].replace(/\{([\s\S]*)\}/, '$1')
     },
